389caec9b6522ec1e7c59918cf69fa69ec8f72c9
Wander128 <57777546+Wander128@users.noreply.github.com>
2020-03-19 01:41:21 -0500

    Juego 0.1
    Prototipo juego

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+        //if (Input.GetButtonDown("Fire1"))
+       // {
+        //    anim.SetTrigger("Push");
+       // }
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            //Quaternion newRotation2 = Quaternion.Slerp(transform.rotation, newRotation, 0.1f);
+            playerRigidbody.MoveRotation(newRotation);
+            //playerRigidbody.MoveRotation(newRotation2);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool running = !((v == 0) && (h == 0));
+        anim.SetBool("IsRunning", running);
+    }
+}
+
---
+++ Assets/Scripts/Player/ZombiePush.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiePush : MonoBehaviour
+{
+    Animator zombMoves;
+    //float yDir;
+
+    void Start()
+    {
+        zombMoves = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            zombMoves.SetTrigger("Push");
+        }
+    }
+}
